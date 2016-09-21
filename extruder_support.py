06db3ef0f5cca2d600a00716b5c0e3a88f046b67
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2016-09-21 21:41:18 +0200

    Added more comments

--- extruder_support.py
+++ extruder_support.py
@@ -1,3 +1,4 @@
+#3d printer for mounting hotend to X-carriage inspired by the P3steel Toolson edition - http://www.thingiverse.com/thing:1054909
 import cadquery as cq
 from Helpers import show
 
@@ -6,47 +7,53 @@ def move_to_center(cqObject,shape):
     Moves the origin of the current Workplane to the center of a given geometry object
     '''
 
-    #transfrom to workplane local coords
+    #transform to workplane local coordinates
     shape_center = shape.Center().sub(cqObject.plane.origin)
-    #project ont plane using dot product
+
+    #project onto plane using dot product
     x_offset = shape_center.dot(cqObject.plane.xDir)
     y_offset = shape_center.dot(cqObject.plane.yDir)
 
     return cqObject.center(x_offset,y_offset)
 
-main_plate_size_y = 67
-main_plate_size_x = 50.
-main_plate_thickness = 10.
+# Parameter definitions
 
-wing_size_x = 10.
-wing_size_y = 10.
+main_plate_size_y = 67 #size of the main plate in y direction
+main_plate_size_x = 50. #size of the main plate in x direction
+main_plate_thickness = 10. #thickness of the main plate
 
-bridge_depth = 35.
+wing_size_x = 10. #size of the side wing supporting the bridge in x direction
+wing_size_y = 10. #size of the side wing supporting the bridge in y direction
 
-support_depth = 18.
+bridge_depth = 35. #depth of the bridge
 
-cutout_depth = 15.
-cutout_rad = 8.
-cutout_offset = 2.
+support_depth = 18. #depth of the bridge support
 
-extruder_hole_spacing = 50.
+cutout_depth = 15. #depth of the hotend cutout
+cutout_rad = 8.    #radius of the cutout (cf groove mount sizes of E3D hotends)
+cutout_offset = 2. #delta radius of the second cutout (cf groove mount sizes of E3D hotends)
 
-m4_predrill = 3.7
-m3_predrill = 2.5
-m3_cbore = 5.
+extruder_hole_spacing = 50. #spacing of the extruder mounting holes (Wade's geared extruder)
 
-mounting_hole_spacing = 28.
+m4_predrill = 3.7 #hole diameter for m4 tapping
+m3_predrill = 2.5 #hole diameter for m3 tapping
+m3_cbore = 5. #counterbore size for m3 socket screw
 
-aux_hole_depth = 6.
-aux_hole_spacing = 5.
-aux_hole_N = 2
+mounting_hole_spacing = 28. #spacing of the mounting holes for attaching to x-carriage
 
-#main plate
+aux_hole_depth = 6. #depth  of the auxiliary holes at the sides of the object
+aux_hole_spacing = 5. #spacing of the auxiliary holes within a group
+aux_hole_N = 2 #number of the auxiliary hole per group
+
+#make the main plate
 res = cq.Workplane('front').box(main_plate_size_x,
                                 main_plate_size_y,
                                 main_plate_thickness)
 
 def add_wing(obj, sign=1):
+    '''
+    Adds a wing to the main plate, defined to keep the code DRY
+    '''
     obj = obj.workplane()\
             .hLine(sign*wing_size_x)\
             .vLine(-wing_size_y)\
@@ -55,22 +62,26 @@ def add_wing(obj, sign=1):
     return obj
 
 #add wings
+
+#add right wing
 res = res.faces('<Z').vertices('>XY')
-res = add_wing(res)  # add right wing
+res = add_wing(res)
 
-#store sides of the plate for further reuse
-
+#store sides of the plate for further reuse, their area is used later on to calculate "optimum" spacing of the aux hole groups
 face_right = res.faces('>X[1]').val()
 face_left  = res.faces('>X[-2]').val()
 
+#add left wing
 res = res.faces('<Z').vertices('>Y').vertices('<X')
-res = add_wing(res, -1)  # add left wing
+res = add_wing(res, -1)
 
-wp = res.faces('>Z')
-e = wp.edges('>Y')
+#make the bridge for extruder mounting
+wp = res.faces('>Z') #select top face
+e = wp.edges('>Y') #select most extreme edge in Y direction
 
-bridge_length = e.val().Length()
+bridge_length = e.val().Length() #the width of the bridge equals to the length of the selected edge
 
+#draw the bridge x-section and extrude
 res = e.vertices('<X'). \
         workplane(). \
         hLine(bridge_length). \
@@ -78,22 +89,25 @@ res = e.vertices('<X'). \
         hLine(-bridge_length). \
         close().extrude(bridge_depth)
 
-faces = res.faces('>Z[1]')
-edge = faces.edges('>Y')
+faces = res.faces('>Z[1]') #take all faces in Z direction and select the middle one; note the new selector syntax
+edge = faces.edges('>Y') #select the top edge of this face...
 res = move_to_center(faces.workplane(),edge.val()).\
-                     transformed(rotate=(0, 90, 0))
+                     transformed(rotate=(0, 90, 0)) #...and make a workplane that is centered in this edge and oriented along X direction
 
 res = res.vLine(-support_depth).\
     line(-support_depth,support_depth).\
-    close()
+    close() #draw a triangle
 
-res = res.extrude(main_plate_size_x/2,both=True,clean=True)
+res = res.extrude(main_plate_size_x/2,both=True,clean=True) #extrude the triangle, now the bridge has a nice support making it much more stiff
 
-face = res.faces('>Y')
-
-res = move_to_center(face.workplane(),face.edges('>Z').val())
+#Start cutting out a slot for hotend mounting
+face = res.faces('>Y') #select the most extreme face in Y direction, i.e. top ot the "bridge"
+res = move_to_center(face.workplane(),face.edges('>Z').val()) #shift the workplane to the center of the most extreme edge of the bridge
 
 def make_slot(obj,depth=None):
+    '''
+    Utility function that makes a slot for hotend mounting
+    '''
     obj = obj.moveTo(cutout_rad,-cutout_depth).\
         threePointArc((0,-cutout_depth-cutout_rad),
                       (-cutout_rad,-cutout_depth)).\
@@ -106,18 +120,18 @@ def make_slot(obj,depth=None):
 
     return obj
 
-res = make_slot(res,None)
+res = make_slot(res,None) #make the smaller slot
 
-cutout_rad += cutout_offset
-res = make_slot(res.end().end(),-main_plate_thickness/2)
+cutout_rad += cutout_offset #increase the cutout radius...
+res = make_slot(res.end().end(),-main_plate_thickness/2) #...and make a slightly larger slot
 
 res = res.end().moveTo(0,0).\
       pushPoints([(-extruder_hole_spacing/2,-cutout_depth),
             (extruder_hole_spacing/2,-cutout_depth)]).\
-      hole(m4_predrill)
+      hole(m4_predrill) #add extruder mounting holes at the top of the bridge
 
 
-
+#make additional slot in the bridge support which allows the hotend's radiator to fit
 cutout_rad += 3*cutout_offset
 res = make_slot(res.end().moveTo(0,0).workplane(offset=-main_plate_thickness))
 
@@ -138,7 +152,7 @@ res = res.workplane(offset=bridge_depth). \
       transformed(rotate=(0,0,90)). \
       polygon(6,30).cutThruAll()
 
-#make 4 mouting holes with cbores
+#make 4 mounting holes with cbores
 res = res.end().moveTo(0,0). \
       rect(mounting_hole_spacing,
            mounting_hole_spacing,forConstruction=True)
@@ -154,6 +168,9 @@ res = res.transformed(rotate=(0,0,45)). \
       rect(25,25,forConstruction=True).vertices().hole(m3_predrill)
 
 def make_aux_holes(workplane,holes_span,N_hole_groups = 3):
+    '''
+    Utility function for creation of auxiliary mouting holes at the sides of the object
+    '''
     res = workplane.moveTo(-holes_span/2).\
          workplane().rarray(aux_hole_spacing,1,aux_hole_N,1).hole(m3_predrill,depth=aux_hole_depth)
     for i in range(N_hole_groups-1):
@@ -168,32 +185,23 @@ res = make_aux_holes(res,main_plate_size_x*2/3.,3)
 #make aux holes at the side (@overhang)
 res = res.faces('<X').workplane().transformed((90,0,0))
 res = make_aux_holes(res,main_plate_size_x*2/3.,3)
-
 res = res.faces('>X').workplane().transformed((90,0,0))
 res = make_aux_holes(res,main_plate_size_x*2/3.,3)
 
 #make aux holes at the side (@main plate)
 res = res.faces('|X').edges('<Y').edges('>X')
-
 res = res.workplane()
 res = move_to_center(res,face_right)
-
 res = res.transformed((90,0,0))
-
 hole_sep = 0.5*face_right.Area()/main_plate_thickness
-
 res = make_aux_holes(res,hole_sep,2)
 
 #make aux holes at the side (@main plate)
 res = res.faces('|X').edges('<Y').edges('<X')
-
 res = res.workplane()
 res = move_to_center(res,face_left)
-
 res = res.transformed((0,180,0))
-
 hole_sep = 0.5*face_right.Area()/main_plate_thickness
-
 res = make_aux_holes(res,hole_sep,2)
 
 #show the result
